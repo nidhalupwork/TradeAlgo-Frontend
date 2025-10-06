7da41dd4290e6695071974e906f2def89e2976b6
nidhalupwork <nidhalupwork@gmail.com>
2025-10-06 10:19:46 +0900

    chore: change backend endpoint to production

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_ENDPOINT = 'https://api.tradealgorithm.co.uk'; // Deploy
-export const BACKEND_ENDPOINT = 'http://localhost:3000'; // Local
+export const BACKEND_ENDPOINT = 'https://api.tradealgorithm.co.uk'; // Deploy
+// export const BACKEND_ENDPOINT = 'http://localhost:3000'; // Local
